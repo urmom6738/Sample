7f8ca440ae5a7350c6d45829d13abae4a02506ea
urmom6738 <lclark980987@gmail.com>
2026-06-09 14:01:50 -0400

    refactor: clean up commented code in opcontrol function

--- src/main.cpp
+++ src/main.cpp
@@ -79,7 +79,10 @@ void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({20, 10});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({-11, -1});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-	//pros::Motor intake_motor(15); sample motor
+	// sample motor for intake
+	pros::Motor intake_motor(15);
+	// sample motor for lift
+	pros::Motor lift_motor(14);
 
 
 	while (true) {
@@ -92,13 +95,19 @@ void opcontrol() {
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
 		left_mg.move(dir - turn);                      // Sets left motor voltage
 		right_mg.move(dir + turn);                     // Sets right motor voltage
-	/* sample motor code
-		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
+	// sample motor code
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
     		intake_motor.move(100);
   		} else {
    			intake_motor.move(0);
   		}
-	*/
+	// sample motor code for lift
+	if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
+    		lift_motor.move(100);
+  		} else {
+   			lift_motor.move(0);
+  		}
+
 
 		pros::delay(20);                               // Run for 20 ms then update
 	}
